96c878249b6a8d72f3629c9f7ad45c10654fe59c
TheQuest1 <madshallenberg1@hotmail.com>
2021-03-10 03:46:47 +0100

    RPG game, done first part of movement, next = first attacks

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour, IAction
+    {
+        [SerializeField] Transform target;
+        NavMeshAgent navMestAgent;
+
+        void Start()
+        {
+            navMestAgent = GetComponent<NavMeshAgent>();
+            
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination);
+        }
+        public void MoveTo(Vector3 destination)
+        {
+            navMestAgent.SetDestination(destination);
+            navMestAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            navMestAgent.isStopped = true;
+        }
+
+    
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMestAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+    }
+}
